bddb741316a1628d028d47c5bc1da55a107c6cbd
lucas-PI <lucas22inacio@hotmail.com>
2024-10-03 22:43:37 -0300

    criaçÃão da classe usuário

---
+++ src/domain/Usuario.java
@@ -0,0 +1,49 @@
+package domain;
+
+import service.ServiceUsuario;
+
+public class Usuario {
+
+    private String nome;
+    private String email;
+    private  String dataNascimento;
+    private  final ServiceUsuario serviceUsuario;
+
+
+    public Usuario(String nome, String email, String dataNascimento) {
+        this.nome = nome;
+        this.email = email;
+        this.dataNascimento = dataNascimento;
+        this.serviceUsuario = new ServiceUsuario();
+    }
+
+    public ServiceUsuario getServiceUsuario() {
+        return serviceUsuario;
+    }
+
+    public String getNome(){
+
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+}
