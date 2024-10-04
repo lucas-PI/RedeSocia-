8cba2bb586a29f9439c72c8edad2de0977e40424
lucas-PI <lucas22inacio@hotmail.com>
2024-10-04 14:18:57 -0300

    regra de negocio da classe usuario, criação do calcula idade

--- src/service/ServiceUsuario.java
+++ src/service/ServiceUsuario.java
@@ -1,4 +1,23 @@
 package service;
 
+import domain.Usuario;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
 public class ServiceUsuario {
+    public String exibirInfo(Usuario usuario){
+        return usuario.toString();
+    }
+    public int Idade(String dataNascimento){
+        DateTimeFormatter formatador =  DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate dataNasicmentoFormatada  = LocalDate.parse(dataNascimento,formatador);
+        return calcularIdade(dataNasicmentoFormatada);
+    }
+
+    public int calcularIdade(LocalDate dataNacismento){
+        LocalDate dataAtual = LocalDate.now();
+        return Period.between(dataNacismento,dataAtual).getYears();
+    }
 }
